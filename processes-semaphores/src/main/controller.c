812c8940263c24d7a434140c32a92f42d8dc4833
Arsen Badalyan <arsenbadalyan.01@gmail.com>
2023-12-30 01:19:13 +0400

    fix: segv in thread created by child process

--- processes-semaphores/src/main/controller.c
+++ processes-semaphores/src/main/controller.c
@@ -12,30 +12,62 @@
 
 #include "philo.h"
 
-void	*wait_eat(void *arg)
+typedef struct threadArgs_s {
+	int		ph_limit;
+	int		*pid_list;
+	sem_t	*f_sem;
+}	t_threadArgs;
+
+t_threadArgs	*copy_thread_args(t_philos *philos)
 {
-	int			i;
-	t_philos	*philos;
+	size_t			i;
+	t_threadArgs	*thread_args;
 
 	i = 0;
-	philos = (t_philos *)arg;
-	while (i < (int)philos->limits->ph_num)
+	thread_args = (t_threadArgs *)malloc(sizeof(t_threadArgs));
+	if (!thread_args)
+		mem_issue(philos);
+	thread_args->ph_limit = philos->limits->ph_num;
+	thread_args->pid_list = (int *)malloc(thread_args->ph_limit * sizeof(int));
+	if (!thread_args->pid_list)
+		mem_issue(philos);
+	while (i < (size_t)thread_args->ph_limit)
 	{
-		waitpid(philos->philo_list[i].pid, NULL, 0);
+		thread_args->pid_list[i] = philos->philo_list[i].pid;
+		++i;
+	}
+	thread_args->f_sem = philos->finish;
+	return (thread_args);
+}
+
+void	*wait_eat(void *args)
+{
+	int				i;
+	t_threadArgs	*philos;
+
+	i = 0;
+	philos = (t_threadArgs *)args;
+	while (i < philos->ph_limit)
+	{
+		waitpid(philos->pid_list[i], NULL, 0);
 		i++;
 	}
-	sem_post(philos->finish);
+	sem_post(philos->f_sem);
+	free(philos->pid_list);
+	free(philos);
 	return (NULL);
 }
 
 void	wait_any_action(t_philos *philos)
 {
-	pthread_t	*thread;
-	int			i;
+	pthread_t		*thread;
+	int				i;
+	void			*args;
 
+	args = (void *)copy_thread_args(philos);
 	thread = (pthread_t *)malloc(sizeof(pthread_t));
 	sem_wait(philos->finish);
-	if (pthread_create(thread, NULL, &wait_eat, (void *)philos))
+	if (pthread_create(thread, NULL, &wait_eat, args))
 		force_quit(E_THREAD, philos);
 	sem_wait(philos->finish);
 	if (pthread_detach(*thread))
